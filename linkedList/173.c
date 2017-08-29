60592d18b78c74cf20bda2e95bfe379b660abb5e
TRY888 <1332981729@qq.com>
2017-08-30 01:02:33 +0800

    添加173，372

---
+++ linkedList/173.c
@@ -0,0 +1,46 @@
+﻿/**
+ * title:173.链表插入排序
+ * desc:用插入排序对链表排序
+ * e.g.:Given 1->3->2->0->null, return 0->1->2->3->null
+ */
+
+/**
+ * Definition of ListNode
+ * class ListNode {
+ * public:
+ *     int val;
+ *     ListNode *next;
+ *     ListNode(int val) {
+ *         this->val = val;
+ *         this->next = NULL;
+ *     }
+ * }
+ */
+class Solution {
+public:
+    /**
+     * @param head: The first node of linked list.
+     * @return: The head of linked list.
+     */
+    ListNode *insertionSortList(ListNode *head) {
+        // write your code here
+        ListNode *p,*s,*q;
+        p = head->next;
+        s = head;
+        q = head;
+        while(p != NULL)
+        {
+        	s = head;
+        	q = head;	
+			while((q != NULL) && (p->val > q->val))
+			{
+				s = q;
+				q = q->next;
+			}
+			s->next = p;
+			p->next = q;		
+        	p = p->next;
+        }
+        return head;
+    }
+};
