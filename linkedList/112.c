877af6ffbe171cb2e6300ad5f3db16beffb4a23c
TRY888 <1332981729@qq.com>
2017-09-04 03:47:09 -0500

    Add files via upload
    添加 35 96 166 112 174

---
+++ linkedList/112.c
@@ -0,0 +1,53 @@
+﻿/*
+ * title：容易-112.删除排序链表中的重复元素
+ * desc:给定一个排序链表，删除所有重复的元素每个元素只留下一个。
+ * e.g.:给出 1->1->2->null，返回 1->2->null
+ *      给出 1->1->2->3->3->null，返回 1->2->3->null
+ */
+
+/**
+ * Definition of ListNode
+ * class ListNode {
+ * public:
+ *     int val;
+ *     ListNode *next;
+ *     ListNode(int val) {
+ *         this->val = val;
+ *         this->next = NULL;
+ *     }
+ * }
+ */
+class Solution {
+public:
+    /**
+     * @param head: The first node of linked list.
+     * @return: head node
+     */
+    ListNode *deleteDuplicates(ListNode *head) {
+        // write your code here
+        if(head == NULL)
+            return NULL;
+        ListNode *p = head;
+        ListNode *q = head;
+        int x = p->val;
+        ListNode *temp;
+        p = p->next;
+        while(p != NULL)
+        {
+            if(p->val == x)
+            {
+                temp = p;
+                q->next = p->next;
+                p = p->next;
+                delete temp;
+            }
+            else
+            {
+                x = p->val;
+                q = p;
+                p = p->next;
+            }
+        }
+        return head;
+    }
+};
---
+++ linkedList/35.c
@@ -0,0 +1,48 @@
+﻿/*
+ * title：容易-35.翻转链表
+ * desc:翻转一个链表
+ * e.g.:给出一个链表1->2->3->null，这个翻转后的链表为3->2->1->null
+ */
+
+/**
+ * Definition of ListNode
+ * 
+ * class ListNode {
+ * public:
+ *     int val;
+ *     ListNode *next;
+ * 
+ *     ListNode(int val) {
+ *         this->val = val;
+ *         this->next = NULL;
+ *     }
+ * }
+ */
+
+
+class Solution {
+public:
+    /*
+     * @param head: n
+     * @return: The new head of reversed linked list.
+     */
+    ListNode * reverse(ListNode * head) {
+        // write your code here
+        ListNode *newHead = new ListNode(0);
+        newHead->next = NULL;
+        ListNode *p,*temp,*s;
+        p = head;
+        s = newHead->next;
+        while(p!=NULL)
+        {
+            newHead->next = p;
+            temp = p->next;
+            p->next = s;
+            s = p;
+            p = temp;
+        }
+        temp = newHead->next;
+        delete newHead;
+        return temp;
+    }
+};
