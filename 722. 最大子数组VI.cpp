3d09dfad6506e358bf22eb78085162631de48d40
TRY888 <1332981729@qq.com>
2017-11-22 02:37:39 -0600

    Upload 722. 最大子数组VI.cpp

---
+++ 722. 最大子数组VI.cpp
@@ -0,0 +1,28 @@
+﻿class Solution {
+public:
+    /*
+     * @param : the array
+     * @return: the max xor sum of the subarray in a given array
+     */
+     
+    // 本方法时间复杂度o(n^2),有时候会提交成功
+    int maxXorSubarray(vector<int> &nums) {
+        // write code here
+        vector<int> temp;
+        int max = INT_MIN;
+        for(int i=0; i<nums.size(); i++)
+        {
+            for(int j=0; j<temp.size(); j++)
+            {
+                int t = nums[i]^temp[j];
+                temp[j] = t;
+                if(t>max)
+                    max = t;
+            }
+            temp.push_back(nums[i]);
+            if(nums[i]>max)
+                max = nums[i];
+        }
+        return max;
+    }
+};
