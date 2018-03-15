0f208f5a7fc3e9958256b5dbfacec31b93e985a5
TRY888 <1332981729@qq.com>
2018-03-15 10:47:35 +0800

    Upload 103. 带环链表 II.cpp

--- 103. 带环链表 II.cpp
+++ 103. 带环链表 II.cpp
@@ -10,6 +10,50 @@
  *     }
  * }
  */
+// class Solution {
+// public:
+//     /**
+//      * @param head: The first node of linked list.
+//      * @return: The node where the cycle begins.
+//      *           if there is no cycle, return null
+//      */
+
+//   //首先，利用快慢指针判断有无环，若遇到slow == fast时，跳出循环；
+//   //然后，调整fast=head，slow不变，此时slow与fast同步移动，直至再次相遇，即是
+//   //表中环的起始节点。
+
+//   ListNode *detectCycle(ListNode *head) {
+//         // write your code here
+//       if(head == NULL || head->next ==NULL)
+//         {
+//             return NULL;
+//         }
+
+//         ListNode *slow = head, *fast = head;
+//         while(fast && fast->next)
+//         {
+//             slow = slow->next;
+//             fast = fast->next->next;
+//             if(slow == fast)
+//             {
+//                 break;
+//             }
+//         }
+
+//         if(fast && fast == slow)
+//         {
+//             fast = head;
+//             while(fast != slow)
+//             {
+//                 fast = fast->next;
+//                 slow = slow->next;
+//             }
+//             return fast;
+//         }
+//         return NULL;
+//     }
+// };
+
 class Solution {
 public:
     /**
@@ -24,26 +68,24 @@ public:
 
    ListNode *detectCycle(ListNode *head) {
         // write your code here
-       if(head == NULL || head->next ==NULL)
-        {
+        if(head==NULL || head->next==NULL)
             return NULL;
-        }
-
-        ListNode *slow = head, *fast = head;
-        while(fast && fast->next)
+        ListNode *fast = head->next;
+        ListNode *slow = head;
+        while(slow!=NULL && fast!=NULL)
         {
-            slow = slow->next;
-            fast = fast->next->next;
-            if(slow == fast)
-            {
+            if(fast == slow)
                 break;
-            }
+            slow = slow->next;
+            if(fast->next!=NULL)
+                fast = fast->next->next;
+            else
+                break;
         }
-
-        if(fast && fast == slow)
+        if(fast != NULL && fast == slow)
         {
             fast = head;
-            while(fast != slow)
+            while(fast!=slow)
             {
                 fast = fast->next;
                 slow = slow->next;
@@ -53,5 +95,3 @@ public:
         return NULL;
     }
 };
-
-
